532d58dce121b7c647e2efe27335fc8be8d6e658
Meerab <munirmeerab@gmail.com>
2026-05-08 18:42:49 +0500

    sidebar added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [ "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#e12e92",
+        secondary: "#bfb5a5",
+        success: "#6abf45",
+      },
+    },
   },
   plugins: [],
-}
-
+};
